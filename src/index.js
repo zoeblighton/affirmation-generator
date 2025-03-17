f2bb04e442070eed42bfa6d586db3bd05bd62d16
zoeblighton <zoeblighton.seo@gmail.com>
2025-03-17 23:44:06 +0000

    Added Javascript and added Typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+function generateAffirmation(event) {
+  event.preventDefault();
 
+  new Typewriter("#affirmation", {
+    strings:
+      "Relax and enjoy life. Whatever I need to know is revealed to me at exactly the right time.",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let affirmationFormElement = document.querySelector(
+  "#affirmation-generator-form"
+);
+affirmationFormElement.addEventListener("submit", generateAffirmation);
