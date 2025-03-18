5867dd67fd986af70999866c05fe400802b57190
zoeblighton <zoeblighton.seo@gmail.com>
2025-03-18 00:15:59 +0000

    AI API integrated

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-function generateAffirmation(event) {
-  event.preventDefault();
-
+function displayAffirmation(response) {
   new Typewriter("#affirmation", {
-    strings:
-      "Relax and enjoy life. Whatever I need to know is revealed to me at exactly the right time.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateAffirmation(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "ad330884483abf7o091c0c43t8ea93ab";
+  let prompt = `Generate a positive affirmation about ${instructionsInput.value}`;
+  let context =
+    "You are a positive affirmations expert and love to write daily affirmations. Your mission is to write a one-to-two sentence affirmation. Make sure to follow the user instructions.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating affirmation");
+  console.log(`prompt: ${prompt}`);
+  console.log(`prompt: ${context}`);
+
+  axios.get(apiUrl).then(displayAffirmation);
 }
 
 let affirmationFormElement = document.querySelector(
